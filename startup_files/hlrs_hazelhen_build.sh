a6ec9c5979e5229d7550067ea4f68bf853035746
Benson Muite <bkmgit@users.noreply.github.com>
2019-02-06 00:43:37 +0200

    Update hlrs_hazelhen_build.sh

--- startup_files/hlrs_hazelhen_build.sh
+++ startup_files/hlrs_hazelhen_build.sh
@@ -45,4 +45,7 @@ echo "Step 3 of 4 steps of login node installation complete"
 
 nohup ./install-tool --build-compiler gnu --target-arch cray --build-onlyosscbtf --openss-prefix ${OSSCBTF_IDIR} --with-cn-cbtf-krell ${CBTF_IDIR}/compute --krell-root-prefix ${KROOT_IDIR}  --with-papi ${PAPI_IDIR} --with-boost ${BOOST_IDIR} --with-mrnet ${KROOT_IDIR} --with-xercesc ${KROOT_IDIR} --with-libmonitor ${KROOT_IDIR} --with-libunwind ${KROOT_IDIR} --with-dyninst ${KROOT_IDIR} --with-libelf ${KROOT_IDIR} --with-libdwarf ${KROOT_IDIR} --with-binutils ${KROOT_IDIR} --cbtf-prefix ${CBTF_IDIR} --with-mpich ${MPICH_IDIR} 
 
+cp /opt/gcc/7.3.0/snos/lib64/libstdc++.so.6 ${KROOT_IDIR}/lib64/.
+cp /opt/gcc/7.3.0/snos/lib64/libstdc++.so.6 ${KROOT_IDIR}/compute/lib64/.
+
 echo "Login node installation complete"
